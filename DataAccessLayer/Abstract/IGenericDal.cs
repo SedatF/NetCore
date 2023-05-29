192c3c58dc55a594a42c14c9e5b2221700d74a39
SEDAT FINDIK <61835588+SedatF@users.noreply.github.com>
2023-05-29 15:41:52 +0200

    Add DataAccessLayer

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T:class
+    {// Generic bir Interfacedir.  <T> where T:class    T Clasa ati tüm değerleri kullanır. 
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetListAll();
+        T GetById(int id);
+        List<T> GetListAll(Expression<Func<T, bool>> filter);
+    }
+}
