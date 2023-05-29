0fdbc447f00c10062e35bc73314ddd7746ff4441
SEDAT FINDIK <61835588+SedatF@users.noreply.github.com>
2023-05-29 15:47:55 +0200

    Adding files

---
+++ CoreDemos/ViewComponents/CommentList.cs
@@ -0,0 +1,34 @@
+﻿using CoreDemos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemos.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment> {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Sedat"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Vedat"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Ahmet"
+                } 
+            };
+            return View(commentvalues);
+        }
+    }
+}
